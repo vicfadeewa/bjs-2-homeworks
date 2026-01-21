8c406d7bd41c2654d3542c6b1d07f48c7a480d7c
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2026-01-21 23:26:45 +0300

    домашнее задание по теме Объекты добавлено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (!this.marks) return;
+  this.marks.push(...marksToAdd);
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) return 0; 
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+function testCase() {
+  let student1 = new Student("Василиса", "женский", 19);
+  student1.setSubject("Algebra");
+  console.log(student1.getAverage()); 
+  student1.addMarks(4, 5, 4, 5);
+  console.log(student1.getAverage()); 
+  console.log(student1);
+
+  let student2 = new Student("Артём", "мужской", 25);
+  student2.setSubject("Geometry");
+  student2.exclude('плохая учёба');
+  console.log(student2);
 }
+
+testCase();
+
