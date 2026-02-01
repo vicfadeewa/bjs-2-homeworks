5607885b765c3c26404ac8f4eab16cc81fb8b60f
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2026-02-01 14:06:14 +0300

    добавлено домашнее задание по теме Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,54 @@
-//Задача № 1
+// Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find(item => item.hash === hash);
+
+    if (objectInCache) {
+      console.log('Из кеша: ' + objectInCache.value);
+      return 'Из кеша: ' + objectInCache.value;
+    }
+
+    const result = func(...args);
+    cache.push({ hash: hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log('Вычисляем: ' + result);
+    return 'Вычисляем: ' + result;
+  }
+
+  return wrapper;
 }
 
-//Задача № 2
+// Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isThrottled = false;
+
+  function wrapper(...args) {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+    }, delay);
+
+    if (!isThrottled) {
+      func.apply(this, args);
+      wrapper.count++;
+      isThrottled = true;
+    }
+
+    wrapper.allCount++;
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
